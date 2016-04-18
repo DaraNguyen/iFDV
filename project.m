5a73c86c4c617db45365328e38acfbd2b37a7f7b
Dara <daralinh@hotmail.com>
2016-04-18 18:12:17 +0200

    first commit

---
+++ project.m
@@ -0,0 +1,25 @@
+clc
+
+% importer le fichier
+
+clear Cum2014
+clear Cum2014flag
+clear Currentweek
+clear Currentweekflag
+clear MMWRweek
+clear MMWRyear
+clear yearweeklyaverage
+clear yearweeklyaverageflag
+
+
+
+% extraire les colonnes et lignes qui nous intéressent
+year=[Totalcasesreported2009 Totalcasesreported2010 Totalcasesreported2011 Totalcasesreported2012 Totalcasesreported2013];
+
+% plot nb de personnes infectées en fonction de l'année
+
+% tracer histogramme (?) de la moyenne des personnes infectées -> à adapter
+
+% pour chaque maladie, calculer le delta(nb de personnes infectées) moyen
+
+% histogramme des variations ?
